3628fbc4a30c273f97f9d996c9a703df1629af01
Mo-eid200 <mo.atta.eid@gmail.com>
2025-07-06 02:17:26 +0400

    Update route.js

--- app/api/services/route.js
+++ app/api/services/route.js
@@ -1,4 +1,3 @@
-export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
@@ -9,7 +8,9 @@ export async function GET() {
   try {
     const querySnapshot = await getDocs(collection(adminFirestore, "services"));
     let services = [];
-    querySnapshot.forEach(doc => services.push({ id: doc.id, ...doc.data() }));
+    querySnapshot.forEach((doc) =>
+      services.push({ id: doc.id, ...doc.data() })
+    );
     return NextResponse.json({ services });
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
