648bf0f86f736dcbaf936493b523c013a1b90bf9
Mo-eid200 <mo.atta.eid@gmail.com>
2025-07-28 00:31:24 +0400

    14

--- components/ClientChat/ChatWidgetFull.js
+++ components/ClientChat/ChatWidgetFull.js
@@ -75,17 +75,6 @@ export default function ChatWidgetFull({
     setStep("faq");
   };
 
-
-async function translateText(text, targetLang) {
-  const res = await fetch('/api/translate', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ text, targetLang }),
-  });
-  const data = await res.json();
-  return data.translatedText;
-}
-
   // بدء محادثة من سؤال FAQ
   const handleFaqSelect = async (q, a) => {
     setStep("chat");
