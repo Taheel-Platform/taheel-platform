0ebe23cc642869cac7ac4ffc618ef6bf75c42c8d
Mo-eid200 <mo.atta.eid@gmail.com>
2025-07-29 04:11:41 +0400

    14

--- app/api/openai-gpt/route.js
+++ app/api/openai-gpt/route.js
@@ -3,8 +3,15 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { initializeApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-  // بيانات مشروعك هنا
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 if (!getApps().length) initializeApp(firebaseConfig);
 
 export async function POST(req) {
