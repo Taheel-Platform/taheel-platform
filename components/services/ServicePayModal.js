0e635c6459709a1986421067ad532357f32e8426
Mo-eid200 <mo.atta.eid@gmail.com>
2025-08-08 02:15:28 +0400

    14

--- components/services/ServicePayModal.js
+++ components/services/ServicePayModal.js
@@ -5,8 +5,8 @@ import { firestore } from "@/lib/firebase.client";
 
 // دالة توليد رقم تتبع بالشكل المطلوب
 function generateOrderNumber() {
-  const part1 = Math.floor(100 + Math.random() * 900);
-  const part2 = Math.floor(100 + Math.random() * 900);
+  const part1 = Math.floor(100 + Math.random() * 900); // 3 أرقام
+  const part2 = Math.floor(1000 + Math.random() * 9000); // 4 أرقام
   return `REQ-${part1}-${part2}`;
 }
 
